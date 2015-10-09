8ec255cdb258f21a36bcbd5a2c8be5afb43e672f
Jion Fairchild <jijiglobe@yahoo.com>
2015-10-09 11:48:49 -0400

    began skeleton file for module.py

---
+++ module.py
@@ -0,0 +1,10 @@
+
+def authenticate(username, password):
+    #returns a boolean that describes whether the user has succesfully logged in.
+
+def makePost(username, title, contents):
+    #adds a post to the databes from username with title = title and contents = contents
+    #returns a boolean representing if the operation was successful
+
+def addToPost(username, title, content):
+    #adds content to content
