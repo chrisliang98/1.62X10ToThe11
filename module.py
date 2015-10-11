befd6e5fdb124e5d6899b83f9322c290013a8d16
Jion Fairchild <jijiglobe@yahoo.com>
2015-10-11 13:52:59 -0400

    added more functions and testing, still missing exception handling

--- module.py
+++ module.py
@@ -11,20 +11,35 @@ def authenticate(username, password):
     #returns a boolean that describes whether the user has succesfully logged in.
 
 def makePost(username, title, contents):
+    ans = c.execute('select * from posts where title = "%s";' % title)
+    for r in ans:
+        return False;
     ans = c.execute('insert into posts values("'+username+'","'+title+'","'+contents+'");')
-    return False;
+    conn.commit()
+    return True;
     #adds a post to the databes from username with title = title and contents = contents
     #returns a boolean representing if the operation was successful
+    #operation will be unsuccessful if a post with the same title already exists
 
 def getPost(title):
     ans = c.execute('select * from posts where title ="%s";' % title) 
     for r in ans:
         return r[2]
+    #returns the content of post with title = title
+    #may only be useful for debugging
 
-def addToPost(username, title, content):
-    return False;
-    #adds content to content
+def getAllPosts():
+    c.execute('select * from posts;')
+    return c.fetchall();
+    #returns a 2d array where the first index represents row id. The second index works as follows:
+    #the 0 index store sthe name of the original poster
+    #the 1 index represents the title of the post
+    #the 2 index stores the contents of the post.
 
-authenticate("jion","password")
-makePost("jion", "This is another test post", "TEST DATA")
-print getPost("This is another test post")
+
+def addToPost(title, content):
+    newContent = " "+getPost(title)+content
+    c.execute('update posts set contents = "%s" where title="%s";'% (newContent,title))
+    conn.commit()
+    return True;
+    #adds content to content of original post and returns a boolean representing wether or not the operation was successful
