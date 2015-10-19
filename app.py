7e178b3a826ece808266b1d8e34984f7f34b123a
Chris Liang <chrisliang98@gmail.com>
2015-10-19 12:23:54 -0400

    touchup

--- app.py
+++ app.py
@@ -177,6 +177,11 @@ def passChange():
         else:
             return redirect(url_for("home"))
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.debug = True
     app.secret_key="c720minusboying"
